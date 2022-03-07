662890f73c169b04bc5032f87a223b7b522c8dfb
EKIA Benjamin <ekiabenjamin@gmail.com>
2022-03-07 10:19:42 +0100

    Jeu memory fais en javascript

---
+++ app.js
@@ -0,0 +1,77 @@
+const cardArray = [{
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/cheeseburger.png'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png'
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png'
+    }
+
+]
+//console.log(cardArray)
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid')
+const cardsChosen = []
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        //console.log(card, 1)
+        gridDisplay.appendChild(card)
+    }
+}
+createBoard()
+
+function flipCard() {
+    console.log(cardArray)
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    console.log('clicked', cardId)
+    console.log(cardsChosen)
+    this.setAttribute('src', cardArray[cardId].img)
+}
